c9f685cf035e79afce812b3d78938a0f2872e09f
PEZO19 <pezo1919@gmail.com>
2016-09-05 11:17:03 +0200

    add maven to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# maven, before: brew install maven
+export PATH="/usr/local/Cellar/maven/3.3.9/bin:$PATH"
