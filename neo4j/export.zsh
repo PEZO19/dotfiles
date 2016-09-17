a64071150d926dcee8078df16ae55230b8a15300
PEZO19 <pezo1919@gmail.com>
2016-09-17 14:08:13 +0200

    add neo4j stuff and zsh aliases

---
+++ neo4j/export.zsh
@@ -0,0 +1,1 @@
+export NEO4J_HOME='/usr/local/Cellar/neo4j/3.0.4/'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,4 +4,8 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias dot='~/.dotfiles/bin/dot'
 
+alias dot.gotodir='~/.dotfiles'
+
 alias python34='/usr/local/Cellar/python34/3.4.4/bin/python3'
+
+alias symlink.to='readlink'
