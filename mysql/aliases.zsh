06a184ee66d8cb88dd7222ab2c7d901bdd7b762c
PEZO19 <pezo1919@gmail.com>
2016-09-20 13:46:31 +0200

    ...

---
+++ mysql/aliases.zsh
@@ -0,0 +1,3 @@
+# Before, for HomeBrew mysql setup:
+alias mysql.HOMEBREW.INIT='mysqld --explicit_defaults_for_timestamp'
+alias mysql.ROOT='mysql -uroot -p'
--- neo4j/aliases.zsh
+++ neo4j/aliases.zsh
@@ -1,3 +1,3 @@
 alias neo4j.conf.edit='vim /usr/local/Cellar/neo4j/3.0.4/libexec/conf/neo4j.conf'
-alias neo4j.log='vim $NEO4J_HOME/libexec/logs/neo4j.log'
-alias neo4j.tailf='tail -f $NEO4J_HOME/libexec/logs/neo4j.log'
+alias neo4j.log.vim='vim $NEO4J_HOME/libexec/logs/neo4j.log'
+alias neo4j.log.tailf='tail -f $NEO4J_HOME/libexec/logs/neo4j.log'
